fe04e23424a7c7840addeee6736c7515fb1eebc9
Bartosz Cwynar <cwynar.bartosz@gmail.com>
2015-05-27 19:46:21 +0200

    media url changed

--- theteammatez/settings.py
+++ theteammatez/settings.py
@@ -105,7 +105,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
